23b0e6e372c4ba1458a7233fe2fa06a439caa3be
Sin Hak <tls7gkr@naver.com>
2021-05-23 00:03:10 +0900

    f

--- src/main/java/com/example/haha/mama/JTest.java
+++ src/main/java/com/example/haha/mama/JTest.java
@@ -4,7 +4,7 @@ public class JTest {
 
     public int foo() {
 
-        System.out.println("Fsff");
+        System.out.println("Fssff");
         System.out.println("f");
         return 5;
     }
