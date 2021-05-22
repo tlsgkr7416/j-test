5b7b11752aada0572e4b8ddb67b08bf9166c0abe
Sin Hak <tls7gkr@naver.com>
2021-05-22 15:21:19 +0900

    f

--- src/main/java/com/example/haha/HahaApplication.java
+++ src/main/java/com/example/haha/HahaApplication.java
@@ -2,11 +2,13 @@ package com.example.haha;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class HahaApplication {
 
 	public static void main(String[] args) {
+		System.out.println("ff");
 		SpringApplication.run(HahaApplication.class, args);
 	}
 
--- src/main/java/com/example/haha/mama/JTest.java
+++ src/main/java/com/example/haha/mama/JTest.java
@@ -1,9 +1,11 @@
-package com.example.haha;
+package com.example.haha.mama;
 
 public class JTest {
 
     public int foo() {
 
+        System.out.println("Ff");
         return 5;
     }
+
 }
