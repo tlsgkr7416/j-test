852c91263b1d12d00be3694471e7ed78b4d18b15
Sin Hak <tls7gkr@naver.com>
2021-05-23 17:38:27 +0900

    f

--- src/main/java/com/example/haha/HahaApplication.java
+++ src/main/java/com/example/haha/HahaApplication.java
@@ -7,12 +7,8 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.stereotype.Component;
 
 @SpringBootApplication
-public class HahaApplication extends SpringBootServletInitializer {
+public class HahaApplication {
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(HahaApplication.class);
-	}
 	public static void main(String[] args) {
 		System.out.println("ff");
 		SpringApplication.run(HahaApplication.class, args);
