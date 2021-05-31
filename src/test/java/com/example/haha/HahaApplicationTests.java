82d6724dd9192248afb16e07be55a88644d841c2
Sin Hak <tls7gkr@naver.com>
2021-05-31 17:18:45 +0900

    g

--- src/test/java/com/example/haha/HahaApplicationTests.java
+++ src/test/java/com/example/haha/HahaApplicationTests.java
@@ -8,6 +8,7 @@ class HahaApplicationTests {
 
 	@Test
 	void contextLoads() {
+		System.out.println("ttttttttttttttttttttttttttttttt");
 	}
 
 }
--- src/test/java/com/example/haha/JTest1Test.java
+++ src/test/java/com/example/haha/JTest1Test.java
@@ -2,15 +2,16 @@ package com.example.haha;
 
 import com.example.haha.mama.JTest;
 import org.junit.jupiter.api.DisplayName;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@ExtendWith(MockitoExtension.class)
-public class JTest1 {
+@ExtendWith(SpringExtension.class)
+public class JTest1Test {
 
     @InjectMocks
     private JTest jTest;
